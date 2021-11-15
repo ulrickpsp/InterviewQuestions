97ef0ec308579e825a5aad0b7ac8d0498bf121ec
ulrickpsp <ulrickpspgo@gmail.com>
2021-11-15 13:38:55 +0100

    Add files via upload

---
+++ PhoneNumberLetterCombinations/letter_combinations.py
@@ -0,0 +1,52 @@
+# Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could
+# represent. Return the answer in any order.
+#
+# A mapping of digit to letters (just like on the telephone buttons) is given below.
+# Note that 1 does not map to any letters.
+#
+
+# Example 1:
+#
+# Input: digits = "23"
+# Output: ["ad","ae","af","bd","be","bf","cd","ce","cf"]
+#
+# Example 2:
+#
+# Input: digits = ""
+# Output: []
+#
+# Example 3:
+#
+# Input: digits = "2"
+# Output: ["a","b","c"]
+
+from typing import List
+
+
+def letterCombinations(digits: str) -> List[str]:
+    if len(digits) == 0:
+        return []
+    res = []
+    setu = []
+    comb = {"2": "abc", "3": "def", "4": "ghi", "5": "jkl", "6": "mno", "7": "pqrs", "8": "tuv", "9": "wxyz"}
+
+    def dfs(i):
+        if i >= len(digits):
+            res.append("".join(setu))
+            return
+        for j in comb[digits[i]]:
+            setu.append(j)
+            dfs(i + 1)
+            setu.pop()
+
+    dfs(0)
+    return res
+
+
+#
+# Main script
+#
+if __name__ == '__main__':
+    s = "34"
+    values = letterCombinations(s)
+    print(values)
